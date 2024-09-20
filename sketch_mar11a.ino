3c264f3c1d6bb73857e61c051954b4c8d042a83e
ChrisCheng24 <91934048+ChrisCheng24@users.noreply.github.com>
2024-09-20 17:37:04 +0800

    Update sketch_mar11a.ino

--- sketch_mar11a.ino
+++ sketch_mar11a.ino
@@ -1,22 +1,22 @@
 void setup() { 
   pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT); 
+  pinMode(6, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
 }
 
 void loop() { 
-  analogWrite(5, 100); 
-  analogWrite(6, LOW); 
-  analogWrite(10, LOW); 
-  analogWrite(11, 100); 
-  delay(330);
+  analogWrite(5, 100);  // Left wheel forward
+  analogWrite(6, LOW);  // Left wheel backward off
+  analogWrite(10, LOW); // Right wheel backward off
+  analogWrite(11, 100); // Right wheel forward
+  delay(330);           // Move forward for 330ms
   
-  analogWrite(5, 100); 
-  analogWrite(6, LOW); 
-  analogWrite(10, 100); 
-  analogWrite(11, LOW); 
-  delay(1000);
+  analogWrite(5, 100);  // Left wheel forward
+  analogWrite(6, LOW);  // Left wheel backward off
+  analogWrite(10, 100); // Right wheel backward
+  analogWrite(11, LOW); // Right wheel forward off
+  delay(1000);          // Turn for 1 second
 
 //  analogWrite(5, 200); 
 //  analogWrite(6, LOW); 
