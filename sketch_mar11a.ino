deb582f1d6556c3b3f245d6e36ed119ec88eeff3
ChrisCheng24 <91934048+ChrisCheng24@users.noreply.github.com>
2024-09-20 17:32:35 +0800

    Add files via upload

---
+++ sketch_mar11a.ino
@@ -0,0 +1,34 @@
+void setup() { 
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT); 
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop() { 
+  analogWrite(5, 100); 
+  analogWrite(6, LOW); 
+  analogWrite(10, LOW); 
+  analogWrite(11, 100); 
+  delay(330); 
+  
+  
+ analogWrite(5, 100); 
+ analogWrite(6, LOW); 
+  analogWrite(10, 100); 
+  analogWrite(11, LOW); 
+  delay(1000);
+
+//  analogWrite(5, 200); 
+//  analogWrite(6, LOW); 
+//  analogWrite(10, ); 
+//  analogWrite(11, LOW); 
+//  delay(3000); 
+  
+
+  //analogWrite(5, 200); 
+//  analogWrite(6, LOW); 
+//  analogWrite(10, LOW); 
+//  analogWrite(11, LOW); 
+//  delay(790);
+} 
